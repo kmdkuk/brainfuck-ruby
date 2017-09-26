ba4d301efa67eac3a9090279e2814791b865c79d
kouki <kouworld0123@gmail.com>
2017-09-26 17:12:43 +0900

    brainfuckの基盤作成

---
+++ brainfuck.rb
@@ -0,0 +1,34 @@
+class brainfuck
+  
+  def run
+  end
+
+  def next #>
+  end
+
+  def prev #<
+  end
+
+  def increment #+
+  end
+
+  def decrement #-
+  end
+
+  def write #.
+  end
+
+  def read #,
+  end
+
+  def loop_start #[
+  end
+
+  def loop_end #]
+  end
+end
+
+
+sorce = ""
+
+
