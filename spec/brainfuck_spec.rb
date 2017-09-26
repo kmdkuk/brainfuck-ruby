cc22ef361551a0a6b5d45315dfabd360940afb28
kouki <kouworld0123@gmail.com>
2017-09-26 23:18:16 +0900

    testの雛形を作った

---
+++ spec/brainfuck_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+require "./brainfuck"
+
+describe Brainfuck do
+  brainfuck = Brainfuck.new
+  it ">で次に行くこと" do
+
+  end
+
+  it "<で前に戻ること" do
+
+  end
+
+  it "+でインクリメントすること" do
+
+  end
+
+  it "-でデクリメントすること" do
+
+  end
+
+  it ".で出力すること" do
+
+  end
+
+  it ",で標準出力から入力を受け取ること" do
+
+  end
+end
